e7babd71ecbc84114f6e2890a97d1fc3f69ac40d
Carlos <Santos>
2020-07-10 12:22:39 +0100

    update c2r for install

--- db/install.sql
+++ db/install.sql
@@ -1,6 +1,6 @@
-INSERT INTO `{c2r-prefix}_modules` (`name`, `folder`, `code`) VALUES ('{c2r-mod-name}', '{c2r-mod-folder}', '{\r\n	\"fa-icon\": \"fa-newspaper\",\r\n	\"img\": \"\",\r\n	\"sub-items\": {\r\n		\"List\": {\r\n			\"url\": \"\"\r\n		},\r\n		\"Add Article\": {\r\n			\"url\": \"add\"\r\n		}\r\n	},\r\n\"sidebar\": true,\r\n\"dropdown\": false\r\n}');
+INSERT INTO `{{ prefix }}_modules` (`name`, `folder`, `code`) VALUES ('{{ mod-name }}', '{c2r-mod-folder}', '{\r\n	\"fa-icon\": \"fa-newspaper\",\r\n	\"img\": \"\",\r\n	\"sub-items\": {\r\n		\"List\": {\r\n			\"url\": \"\"\r\n		},\r\n		\"Add Article\": {\r\n			\"url\": \"add\"\r\n		}\r\n	},\r\n\"sidebar\": true,\r\n\"dropdown\": false\r\n}');
 
-CREATE TABLE `{c2r-prefix}_7_articles` (
+CREATE TABLE `{{ prefix }}_7_articles` (
 	`id` int(11) NOT NULL,
 	`code` text NOT NULL,
 	`category_id` int(11) NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE `{c2r-prefix}_7_articles` (
 	`date_update` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-CREATE TABLE `{c2r-prefix}_7_articles_lang` (
+CREATE TABLE `{{ prefix }}_7_articles_lang` (
 	`id` int(11) NOT NULL,
 	`article_id` int(11) NOT NULL,
 	`lang_id` int(11) NOT NULL,
@@ -20,15 +20,15 @@ CREATE TABLE `{c2r-prefix}_7_articles_lang` (
 	`meta-description` text NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-ALTER TABLE `{c2r-prefix}_7_articles`
+ALTER TABLE `{{ prefix }}_7_articles`
 	ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `{c2r-prefix}_7_articles_lang`
+ALTER TABLE `{{ prefix }}_7_articles_lang`
 	ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `{c2r-prefix}_7_articles`
+ALTER TABLE `{{ prefix }}_7_articles`
 	MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
-ALTER TABLE `{c2r-prefix}_7_articles_lang`
+ALTER TABLE `{{ prefix }}_7_articles_lang`
 	MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
